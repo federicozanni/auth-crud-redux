257641f3144c7440b1218058622e7f93b8795244
Federico <fedezanni7@gmail.com>
2021-09-13 21:35:53 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,17 +4,4 @@ const db = axios.create({
   baseURL: 'https://react-native-cafe.herokuapp.com/api'
 })
 
-db.interceptors.request.use(
-
-  async(config) => {
-    const token = window.localStorage.getItem('token');
-
-    if (token) {
-        config.headers['x-token'] = token;
-    }
-    
-    return config;
-  }
-)
-
 export default db;
